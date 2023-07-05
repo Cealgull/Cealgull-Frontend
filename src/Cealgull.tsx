841a0c70560b64a9db8960c21d38cbe7048b22aa
BojunRen <albert.cauchy725@gmail.com>
2023-07-05 14:35:06 +0800

    fix: edit App index

--- src/Cealgull.tsx
+++ src/Cealgull.tsx
@@ -1,30 +1,10 @@
-import { HomeView } from "@src/views/HomeView";
-import { PersonView } from "@src/views/PersonView";
-import { PublishView } from "@src/views/PublishView";
-import { TopicView } from "@src/views/TopicView";
-import { RootTabParamList } from "@src/@types/navigation";
+import { Text } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
-const Tab = createBottomTabNavigator<RootTabParamList>();
 
 export default function Cealgull() {
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={{
-            tabBarStyle: { display: "none" },
-            headerShown: false,
-          }}
-        >
-          <Tab.Screen name="Home" component={HomeView}></Tab.Screen>
-          <Tab.Screen name="Person" component={PersonView}></Tab.Screen>
-          <Tab.Screen name="Publish" component={PublishView}></Tab.Screen>
-          <Tab.Screen name="Topic" component={TopicView}></Tab.Screen>
-        </Tab.Navigator>
-      </NavigationContainer>
+      <Text>Cealgull App</Text>
     </SafeAreaProvider>
   );
 }
