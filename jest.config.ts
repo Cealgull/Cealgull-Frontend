fe0d3d54ed008c3d0e198295a2b7ee2ad58343b1
Bojun Ren <bj_Ren@outlook.com>
2023-08-23 16:21:15 +0800

    test: configure jest to be compatible with miragejs

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,21 @@
 import type { Config } from "jest";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const jestPreset = require("@testing-library/react-native/jest-preset");
 
 const config: Config = {
+  collectCoverage: true,
+  verbose: false,
   preset: "jest-expo",
-  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
+  setupFiles: [...jestPreset.setupFiles],
+  setupFilesAfterEnv: [
+    "@testing-library/jest-native/extend-expect",
+    "./jest.setup.js",
+  ],
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?|@rneui|@noble)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
   ],
+  collectCoverageFrom: ["src/**/{!(*.d.ts),}.{ts,js,.tsx,.jsx}", "App.tsx"],
+  coveragePathIgnorePatterns: ["mirage.(js|ts)"],
 };
 
 module.exports = config;
---
+++ jest.setup.js
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+global.self = global;
+// global.window = {};
+global.XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+global.Headers = undefined;
+global.Request = undefined;
+global.Response = undefined;
