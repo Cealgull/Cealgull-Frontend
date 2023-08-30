d12d393efe456537f58a00187098f7d3e98fd872
Yiyang Wu <toolmanp@outlook.com>
2023-08-31 02:48:15 +0800

    refactor(turbo): move Native Binding into specs
    Add index.ts for module exporting
    Remove NativeModule.ts
    
    Signed-off-by: Yiyang Wu <toolmanp@outlook.com>

--- src/native/NativeModule.ts
+++
@@ -1,8 +0,0 @@
-import type { TurboModule } from "react-native";
-import { TurboModuleRegistry } from "react-native";
-
-export interface Spec extends TurboModule {
-  readonly reverseString: (input: string) => string;
-}
-
-export default TurboModuleRegistry.getEnforcing<Spec>("NativeModule");
