3cb1ed0f54e9d97e1230fa04eb6fa26a69a51391
Bojun Ren <bj_Ren@outlook.com>
2023-09-06 20:19:20 +0800

    fix: interaction with (react-native-)dotenv
    Since react-native doesn't support dotenv natively,
    we have to use react-native-dotenv.
    see https://github.com/goatandsheep/react-native-dotenv.
    
    However react-native-dotenv acts a little strange when testing.
    If react-native-dotenv is enabled in babel configuration,
    it will always loads .env into `@dot-env` and even `process.env`,
    which means the true dotenv would fail to load from .env.vault,
    according to different environment variable `DOTENV_KEY`.
    
    To resolve this problem, it should be guaranteed that when running
    tests, .env doesn't exist.
    If needed, run jest with `--no-cache` to clear babel cache.
    
    Signed-off-by: Bojun Ren <bj_Ren@outlook.com>

--- src/services/api.config.ts
+++ src/services/api.config.ts
@@ -3,13 +3,24 @@ import APIUri from "./api_uri.json";
 
 const APIConfig: Record<string, string> = {};
 
-for (const [key, value] of Object.entries(APIUri)) {
-  let auth_api = AUTH_API;
-  let forum_api = FORUM_API;
+function findAPI(): [string, string] {
   if (process.env["NODE_ENV"] === "test") {
-    auth_api = process.env["AUTH_API"] as string;
-    forum_api = process.env["AUTH_API"] as string;
+    // Only works when:
+    //    1. the file .env doesn't exist
+    //    2. (if needed) run jest with `--no-cache`
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    require("dotenv").config();
+    return [
+      process.env["AUTH_API"] as string,
+      process.env["FORUM_API"] as string,
+    ];
   }
+  return [AUTH_API, FORUM_API];
+}
+
+const [auth_api, forum_api] = findAPI();
+
+for (const [key, value] of Object.entries(APIUri)) {
   if (/^auth\./g.test(key)) {
     APIConfig[key] = `${auth_api}${value}`;
   } else {
