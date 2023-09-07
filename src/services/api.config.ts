a1c9d7c4d59fe1a740379889ec07caef7b3b454d
Bojun Ren <bj_Ren@outlook.com>
2023-09-07 13:31:53 +0800

    fix: interaction with (react-native-)dotenv (#23)
    CAVEAT: this commit adds npm script hooks,
    including prestart, poststart and pretest,
    which may cause `yarn test` fails at the very beginning.
    Try testing after removing the file ".env".
    
    Since react-native doesn't support dotenv natively,
    we have to use react-native-dotenv.
    see https://github.com/goatandsheep/react-native-dotenv.
    
    However react-native-dotenv acts a little strange when testing.
    If react-native-dotenv is enabled in babel configuration,
    it will always loads .env into `@dot-env` and even `process.env`,
    which means the true dotenv would fail to load from .env.vault,
    according to different environment variable `DOTENV_KEY`.
    
    To resolve this problem, it should be guaranteed that
    when running tests, .env doesn't exist.
    If needed, run jest with `--no-cache` to clear babel cache.
    
    Even `dotenv/config` is required conditionally,
    the project fails to build react native environment.
    Therefore, only import it in the mock file.
    
    Signed-off-by: Bojun Ren <bj_Ren@outlook.com>

--- src/services/api.config.ts
+++ src/services/api.config.ts
@@ -4,16 +4,10 @@ import APIUri from "./api_uri.json";
 const APIConfig: Record<string, string> = {};
 
 for (const [key, value] of Object.entries(APIUri)) {
-  let auth_api = AUTH_API;
-  let forum_api = FORUM_API;
-  if (process.env["NODE_ENV"] === "test") {
-    auth_api = process.env["AUTH_API"] as string;
-    forum_api = process.env["AUTH_API"] as string;
-  }
   if (/^auth\./g.test(key)) {
-    APIConfig[key] = `${auth_api}${value}`;
+    APIConfig[key] = `${AUTH_API}${value}`;
   } else {
-    APIConfig[key] = `${forum_api}${value}`;
+    APIConfig[key] = `${FORUM_API}${value}`;
   }
 }
 
