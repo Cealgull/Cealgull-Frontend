331decc86ae0236670a87df7a89fc098e4396c8b
Bojun Ren <bj_Ren@outlook.com>
2023-09-07 13:14:21 +0800

    revert: "fix: interaction with (react-native-)dotenv (#20)"
    This reverts commit f6579c721e9eb82225b1be5a3b9f934fd55d9c94.

--- src/services/api.config.ts
+++ src/services/api.config.ts
@@ -3,24 +3,13 @@ import APIUri from "./api_uri.json";
 
 const APIConfig: Record<string, string> = {};
 
-function findAPI(): [string, string] {
-  if (process.env["NODE_ENV"] === "test") {
-    // Only works when:
-    //    1. the file .env doesn't exist
-    //    2. (if needed) run jest with `--no-cache`
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    require("dotenv").config();
-    return [
-      process.env["AUTH_API"] as string,
-      process.env["FORUM_API"] as string,
-    ];
-  }
-  return [AUTH_API, FORUM_API];
-}
-
-const [auth_api, forum_api] = findAPI();
-
 for (const [key, value] of Object.entries(APIUri)) {
+  let auth_api = AUTH_API;
+  let forum_api = FORUM_API;
+  if (process.env["NODE_ENV"] === "test") {
+    auth_api = process.env["AUTH_API"] as string;
+    forum_api = process.env["AUTH_API"] as string;
+  }
   if (/^auth\./g.test(key)) {
     APIConfig[key] = `${auth_api}${value}`;
   } else {
